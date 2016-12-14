82bc81d0abd9b50dcdaf796c73a7a3d988a9e58c
Pavel Vlasanek <tucnaq@gmail.com>
2016-12-14 12:51:49 +0100

    regular commit

--- modules/fuzzy/include/opencv2/fuzzy/fuzzy_F0_math.hpp
+++ modules/fuzzy/include/opencv2/fuzzy/fuzzy_F0_math.hpp
@@ -122,6 +122,7 @@ namespace ft
     CV_EXPORTS_W int FT02D_iteration(InputArray matrix, InputArray kernel, OutputArray output, InputArray mask, OutputArray maskOutput, bool firstStop);
 
     CV_EXPORTS_W void FT02D_FL_process(InputArray matrix, const int radius, OutputArray output);
+	CV_EXPORTS_W void FT02D_FL_process_float(InputArray matrix, const int radius, OutputArray output);
 
     //! @}
 }
